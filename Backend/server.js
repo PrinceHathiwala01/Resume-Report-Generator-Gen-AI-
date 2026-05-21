328d69d4d596d9e0a6a00eb5eadd728fbbe60a07
Prince Hathiwala <princehathiwala76@gmail.com>
2026-05-21 18:32:56 +0530

    Removed .env from git tracking

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const app = require('./src/app');
 const connectDB = require("./src/config/db");
+const invokeGeminiAi = require("./src/services/ai.service");
 connectDB();
+invokeGeminiAi();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,5 +1,16 @@
+const dotenv = require("dotenv").config();
 const { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({
     apiKey:process.env.GOOGLE_GENAI_API_KEY
 });
+
+async function invokeGeminiAi() {
+        const response = await ai.models.generateContent({
+            model:"gemini-2.5-flash",
+            contents:"Hello gemini! Explain what is interview?"
+        })
+        console.log(response.text);
+}
+
+module.exports = invokeGeminiAi;
