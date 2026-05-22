6504966952d57167c395b71651ed47d589f20a60
Prince Hathiwala <princehathiwala76@gmail.com>
2026-05-22 18:20:55 +0530

    .

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -60,6 +60,6 @@ Few models
 gemini-2.5-flash-lite
 gemini-2.5
 
-If the error is raising that your API key is invalid and it is leaked then we need to change the .env file mean github is an open platform so anyone can use our link so 
+If the error is raising that your API key is invalid and it is leaked then we need to change the .env file means github is an open platform so anyone can use our link so 
 we need to put the .env file in the .gitignore file so it will not be pushed to github
 */
