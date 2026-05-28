d4e602893978bab196799c54348254602db62b17
Prince Hathiwala <princehathiwala76@gmail.com>
2026-05-28 14:21:12 +0530

    .

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -102,7 +102,7 @@ async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
+        executablePath: await chromium.executablePath,
         headless: true,
     });
 
